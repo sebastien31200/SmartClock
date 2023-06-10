3d0cbdebfb28d715f0ec7bf529c5a14daf4d70cf
Sebastien Aubourg <sebastien.aubourg31@gmail.com>
2023-06-10 15:16:48 +0200

    Adapt to new Tisseo API

--- vue-app/src/components/page2/Page2Utils.js
+++ vue-app/src/components/page2/Page2Utils.js
@@ -2,5 +2,5 @@ export const apiSettings = {
   jcDecauxApiUrl: "https://api.jcdecaux.com/vls/v3/stations/",
   apiKey: "23c3130ef1e50f8d77883313d038bbe163080587",
   tisseoApiUrl:
-    "https://api.tisseo.fr/v1/stops_schedules.json?key=71c816b8-86e0-464c-b11c-576cc47bafab&stopPointId=3377699720883015",
+    "https://api.tisseo.fr/v2/stops_schedules.json?key=71c816b8-86e0-464c-b11c-576cc47bafab&stopAreaId=stop_area:SA_1631",
 };
